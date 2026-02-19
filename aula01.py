85e4540456eb6cd2a7daeca9ba962aa987cd9457
Ayumi Miyanaga <miyanagaayumi@gmail.com>
2026-02-19 10:04:49 -0300

    Primeira aula de programação

---
+++ aula01.py
@@ -0,0 +1,9 @@
+idade = 16 # variavel inteira
+peso = 45.5 # variavel float
+nome = "Ayumi Miyanaga" # variavel string
+sexo_feminino = True # variavel bool 
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo_feminino)
