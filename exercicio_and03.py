904651984c2da07e771e99c9c8a5b696b2077cad
Ayumi Miyanaga <miyanagaayumi@gmail.com>
2026-02-27 09:56:29 -0300

    aaaaa

--- exercicio_and03.py
+++ exercicio_and03.py
@@ -1,1 +1,7 @@
 nível = int(input("digite seu nível: "))
+esferas = int(input("digite a quantidade de esferas coletadas: "))
+
+if nível >= 20 and esferas > 50:
+    print("Habilidade Super Salto desbloqueada!")
+elif nível < 20 and esferas < 50:
+    print("Requisitos insuficientes para nova habilidade.")
