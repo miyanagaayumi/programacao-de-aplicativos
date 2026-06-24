f623e3ee944756740f08a7a03a0d52529d5d2599
ayumi <miyanagaayumi@gmail.com>
2026-06-24 10:11:41 -0300

    24/06

--- bancocompleto.py
+++ bancocompleto.py
@@ -6,17 +6,15 @@ cursor = conexao.cursor()
 print("Passo 1: Conectado ao banco de dados") 
 
 cursor.execute(''' 
-    CREATE TABLE IF NOT EXISTS alunos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT NOT NULL,
-        telefone TEXT,
-        turma TEXT,
-        idade INTEGER, 
-        cpf TEXT UNIQUE NOT NULL,
-        professor_id INTEGER,
-         
-    )
-''') 
+                CREATE TABLE IF NOT EXISTS alunos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                telefone TEXT,
+                turma TEXT,
+                idade INTEGER, 
+                cpf TEXT UNIQUE NOT NULL,
+                professor_id INTEGER
+                )''') 
 print("passo 2: tabela e campos configurados")
 
 nome_aluno = input("nome: ") 
@@ -34,42 +32,41 @@ conexao.commit()
 conexao.close() 
 
 def listar_alunos():
-
-conexao = sqlite3.connect('escola_demonstracao.db')
-cursor = conexao.cursor()
-cursor.execute(''' SELECT * FROM alunos ''')
-alunos = cursor.fetchall()
-if not alunos:
-    print("nenhum aluno cadastrado")
-else:
-    for aluno in alunos:
-        print(f"id = {aluno[0]}, nome = {aluno[1]}, idade = {aluno[2]}")
-conexao.close()
+    conexao = sqlite3.connect('escola_demonstracao.db')
+    cursor = conexao.cursor()
+    cursor.execute(''' SELECT * FROM alunos ''')
+    alunos = cursor.fetchall()
+    if not alunos:
+        print("nenhum aluno cadastrado")
+    else:
+        for aluno in alunos:
+            print(f"id = {aluno[0]}, nome = {aluno[1]}, idade = {aluno[2]}")
+    conexao.close()
 
 
 
 
 def alterar_nome_e_CPF():
-conexao = sqlite3.connect("escola_demonstracao.db")
-cursor = conexao.cursor()
+    conexao = sqlite3.connect("escola_demonstracao.db")
+    cursor = conexao.cursor()
 
-id_aluno = 1
+    id_aluno = 1
 
-cursor.execute("SELECT * FROM Alunos WHERE id = ?", (id_aluno,))
-print(f"\nantes da alteração: {nome_aluno}")
-print(cursor.fetchone())
-novo_nome = "João Pedro da Silva"
-novo_cpf = "123.456.789-00"
-cursor.execute('''
-    UPDATE Alunos
-    SET nome = ?, cpf = ?
-    WHERE id = ?
-''', (novo_nome, novo_cpf, id_aluno))
-conexao.commit()
+    cursor.execute("SELECT * FROM Alunos WHERE id = ?", (id_aluno,))
+    print(f"\nantes da alteração: {nome_aluno}")
+    print(cursor.fetchone())
+    novo_nome = "João Pedro da Silva"
+    novo_cpf = "123.456.789-00"
+    cursor.execute('''
+        UPDATE Alunos
+        SET nome = ?, cpf = ?
+        WHERE id = ?
+    ''', (novo_nome, novo_cpf, id_aluno))
+    conexao.commit()
 
-cursor.execute("SELECT * FROM Alunos WHERE id = ?", (id_aluno,))
-print("\ndepois da alteração:")
-print(cursor.fetchone())
+    cursor.execute("SELECT * FROM Alunos WHERE id = ?", (id_aluno,))
+    print("\ndepois da alteração:")
+    print(cursor.fetchone())
 
 conexao.close()
 
