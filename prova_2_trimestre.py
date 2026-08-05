d25d41812a223a9e07b0874aefe7082bf6ce003e
ayumi <miyanagaayumi@gmail.com>
2026-08-05 09:24:03 -0300

    05/08

--- prova_2_trimestre.py
+++ prova_2_trimestre.py
@@ -13,22 +13,26 @@ def cadastrar_hospitais():
                     )
                 ''')
 
-    nome_hospital = input("digite o nome do hospital: ")
-    cidade_hospital = input("digite a cidade que esta localizado o hopistal: ")
+    try:
+        nome_hospital = input("digite o nome do hospital: ")
+        cidade_hospital = input("digite a cidade que esta localizado o hopistal: ")
 
-    comando_inserir = f'''INSERT INTO hospitais (nome_hospital, cidade_hospital)
-                        VALUES('{nome_hospital}', '{cidade_hospital}')'''
-    cursor.execute(comando_inserir)
-    conexao.commit()
-    conexao.close()
+        comando_inserir = f'''INSERT INTO hospitais (nome_hospital, cidade_hospital)
+                            VALUES('{nome_hospital}', '{cidade_hospital}')'''
+        cursor.execute(comando_inserir)
+        print("hospital cadastrado!")
+        conexao.commit()
+    except ValueError:
+        print("digite apenas o nome!")
+    except sqlite3.IntegrtyError as e:
+        print("Erro! Informações ja cadastradas!", e)
 
-cadastrar_hospitais()
-print("hospital cadastrado!")
 
 
 def cadastrar_medicos():
     conexao = sqlite3.connect('sistema_hospitais.db')
     cursor = conexao.cursor()
+
     cursor.execute('''
                     CREATE TABLE IF NOT EXISTS medicos (
                     id_medico INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT,
@@ -40,16 +44,24 @@ def cadastrar_medicos():
     try:
         nome_medico = input("digite o seu nome: ")
         crm = int(input("digite seu CRM: "))
+        id_hospital = int(input("digite o id do hospital: "))
+
+        comando_inserir = f'''INSERT INTO medicos (nome_medico, crm, id_hospital)
+                            VALUES('{nome_medico}', {crm}, {id_hospital} )'''
+        cursor.execute(comando_inserir)
+
+        print("médico cadastrado!")
+        conexao.commit()
+
     except ValueError:
         print("Erro! digite apenas o numero do ID")
-    comando_inserir = f'''INSERT INTO medicos (nome_medico, crm, id_hospital)
-                        VALUES('{nome_medico}', {crm}, {id_hospital} )'''
-    cursor.execute(comando_inserir)
-    conexao.commit()
-    conexao.close()
-cadastrar_medicos()
-print("medico cadastrado!")
+    
+    finally:
+        conexao.close()
 
+cadastrar_hospitais()
+cadastrar_medicos()
 
 # EXERCICIO 2
 
+   
